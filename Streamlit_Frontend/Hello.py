7a81ff619e11219ebaed032f5d877043c12b8795
Prisha Sadasivan <prisha001@e.ntu.edu.sg>
2025-04-14 23:41:55 +0800

    Update Hello.py

--- Streamlit_Frontend/Hello.py
+++ Streamlit_Frontend/Hello.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Diet Recommendation System! 👋")
+st.write("# Welcome NTU to Diet Recommendation System! 👋")
 
 st.sidebar.success("Select a recommendation app.")
 
